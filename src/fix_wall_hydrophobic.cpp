5152400f014f4e4ad90285b18980fa195e05ffe3
PabloPiaggi <ppiaggi@gmail.com>
2017-10-02 18:26:44 +0200

    Scheme and input; force missing

--- src/fix_wall_hydrophobic.cpp
+++ src/fix_wall_hydrophobic.cpp
@@ -11,18 +11,138 @@
    See the README file in the top-level LAMMPS directory.
 ------------------------------------------------------------------------- */
 
-#include <math.h>
+#include <stdlib.h>
+#include <string.h>
 #include "fix_wall_hydrophobic.h"
 #include "atom.h"
+#include "comm.h"
+#include "update.h"
+#include "modify.h"
+#include "domain.h"
+#include "lattice.h"
+#include "input.h"
+#include "variable.h"
 #include "error.h"
+#include "force.h"
+#include <math.h>
+#include "respa.h"
 
 using namespace LAMMPS_NS;
 using namespace FixConst;
 
+enum{XLO=0,XHI=1,YLO=2,YHI=3,ZLO=4,ZHI=5};
+enum{NONE=0,EDGE,CONSTANT,VARIABLE};
+
+
 /* ---------------------------------------------------------------------- */
 
 FixWallHydrophobic::FixWallHydrophobic(LAMMPS *lmp, int narg, char **arg) :
-  FixWall(lmp, narg, arg) {}
+  Fix(lmp, narg, arg),
+  nwall(0)
+{
+  scalar_flag = 1;
+  vector_flag = 1;
+  global_freq = 1;
+  extscalar = 1;
+  extvector = 1;
+  respa_level_support = 1;
+  ilevel_respa = 0;
+  virial_flag = 1;
+
+  // parse args
+
+  int scaleflag = 1;
+  fldflag = 0;
+  int pbcflag = 0;
+
+  for (int i = 0; i < 6; i++) xstr[i] = estr[i] = sstr[i] = NULL;
+
+  int iarg = 3;
+  while (iarg < narg) {
+    if ((strcmp(arg[iarg],"xlo") == 0) || (strcmp(arg[iarg],"xhi") == 0) ||
+        (strcmp(arg[iarg],"ylo") == 0) || (strcmp(arg[iarg],"yhi") == 0) ||
+        (strcmp(arg[iarg],"zlo") == 0) || (strcmp(arg[iarg],"zhi") == 0)) {
+      if (iarg+5 > narg) error->all(FLERR,"Illegal fix wall command");
+
+      int newwall;
+      if (strcmp(arg[iarg],"xlo") == 0) newwall = XLO;
+      else if (strcmp(arg[iarg],"xhi") == 0) newwall = XHI;
+      else if (strcmp(arg[iarg],"ylo") == 0) newwall = YLO;
+      else if (strcmp(arg[iarg],"yhi") == 0) newwall = YHI;
+      else if (strcmp(arg[iarg],"zlo") == 0) newwall = ZLO;
+      else if (strcmp(arg[iarg],"zhi") == 0) newwall = ZHI;
+
+      for (int m = 0; (m < nwall) && (m < 6); m++)
+        if (newwall == wallwhich[m])
+          error->all(FLERR,"Wall defined twice in fix wall command");
+
+      wallwhich[nwall] = newwall;
+      if (strcmp(arg[iarg+1],"EDGE") == 0) {
+        xstyle[nwall] = EDGE;
+        int dim = wallwhich[nwall] / 2;
+        int side = wallwhich[nwall] % 2;
+        if (side == 0) coord0[nwall] = domain->boxlo[dim];
+        else coord0[nwall] = domain->boxhi[dim];
+      } else if (strstr(arg[iarg+1],"v_") == arg[iarg+1]) {
+        xstyle[nwall] = VARIABLE;
+        int n = strlen(&arg[iarg+1][2]) + 1;
+        xstr[nwall] = new char[n];
+        strcpy(xstr[nwall],&arg[iarg+1][2]);
+      } else {
+        xstyle[nwall] = CONSTANT;
+        coord0[nwall] = force->numeric(FLERR,arg[iarg+1]);
+      }
+
+      if (strstr(arg[iarg+2],"v_") == arg[iarg+2]) {
+        int n = strlen(&arg[iarg+2][2]) + 1;
+        estr[nwall] = new char[n];
+        strcpy(estr[nwall],&arg[iarg+2][2]);
+        estyle[nwall] = VARIABLE;
+      } else {
+        epsilon[nwall] = force->numeric(FLERR,arg[iarg+2]);
+        estyle[nwall] = CONSTANT;
+      }
+
+      if (strstr(arg[iarg+3],"v_") == arg[iarg+3]) {
+        int n = strlen(&arg[iarg+3][2]) + 1;
+        sstr[nwall] = new char[n];
+        strcpy(sstr[nwall],&arg[iarg+3][2]);
+        sstyle[nwall] = VARIABLE;
+      } else {
+        sigma[nwall] = force->numeric(FLERR,arg[iarg+3]);
+        sstyle[nwall] = CONSTANT;
+      }
+
+      lambda[nwall] = force->numeric(FLERR,arg[iarg+4]);
+      sigmaS[nwall] = force->numeric(FLERR,arg[iarg+5]);
+      cutoff[nwall] = force->numeric(FLERR,arg[iarg+6]);
+      nwall++;
+      iarg += 7;
+
+    } else if (strcmp(arg[iarg],"units") == 0) {
+      if (iarg+2 > narg) error->all(FLERR,"Illegal fix wall command");
+      if (strcmp(arg[iarg+1],"box") == 0) scaleflag = 0;
+      else if (strcmp(arg[iarg+1],"lattice") == 0) scaleflag = 1;
+      else error->all(FLERR,"Illegal fix wall command");
+      iarg += 2;
+    } else if (strcmp(arg[iarg],"fld") == 0) {
+      if (iarg+2 > narg) error->all(FLERR,"Illegal fix wall command");
+      if (strcmp(arg[iarg+1],"no") == 0) fldflag = 0;
+      else if (strcmp(arg[iarg+1],"yes") == 0) fldflag = 1;
+      else error->all(FLERR,"Illegal fix wall command");
+      iarg += 2;
+    } else if (strcmp(arg[iarg],"pbc") == 0) {
+      if (iarg+2 > narg) error->all(FLERR,"Illegal fix wall command");
+      if (strcmp(arg[iarg+1],"yes") == 0) pbcflag = 1;
+      else if (strcmp(arg[iarg+1],"no") == 0) pbcflag = 0;
+      else error->all(FLERR,"Illegal fix wall command");
+      iarg += 2;
+    } else error->all(FLERR,"Illegal fix wall command");
+  }
+
+  size_vector = nwall;
+
+}
 
 /* ---------------------------------------------------------------------- */
 
@@ -47,7 +167,7 @@ void FixWallHydrophobic::precompute(int m)
 
 void FixWallHydrophobic::wall_particle(int m, int which, double coord)
 {
-  double delta,rinv,r2inv,r6inv,fwall;
+  double delta,rinv,r2inv,r6inv,fwall,W0,W1;
   double vn;
 
   double **x = atom->x;
@@ -73,7 +193,8 @@ void FixWallHydrophobic::wall_particle(int m, int which, double coord)
       rinv = 1.0/delta;
       r2inv = rinv*rinv;
       r6inv = r2inv*r2inv*r2inv;
-      fwall = side * r6inv*(coeff1[m]*r6inv - coeff2[m]) * rinv;
+      if (delta ) W0 = side * r6inv*(coeff1[m]*r6inv - coeff2[m]) * rinv;
+      fwall = W0 + lambda[m]*W1;
       f[i][dim] -= fwall;
       ewall[0] += r6inv*(coeff3[m]*r6inv - coeff4[m]) - offset[m];
       ewall[m+1] += fwall;
@@ -85,5 +206,192 @@ void FixWallHydrophobic::wall_particle(int m, int which, double coord)
       }
     }
 
-  if (onflag) error->one(FLERR,"Particle on or inside fix wall surface");
+  if (onflag) error->one(FLERR,"Particle on or inside fix wall/hydrophobic surface");
+}
+
+/* ---------------------------------------------------------------------- */
+
+FixWallHydrophobic::~FixWallHydrophobic()
+{
+  if (copymode) return;
+
+  for (int m = 0; m < nwall; m++) {
+    delete [] xstr[m];
+    delete [] estr[m];
+    delete [] sstr[m];
+  }
+}
+
+/* ---------------------------------------------------------------------- */
+
+int FixWallHydrophobic::setmask()
+{
+  int mask = 0;
+
+  // FLD implicit needs to invoke wall forces before pair style
+
+  if (fldflag) mask |= PRE_FORCE;
+  else mask |= POST_FORCE;
+
+  mask |= THERMO_ENERGY;
+  mask |= POST_FORCE_RESPA;
+  mask |= MIN_POST_FORCE;
+  return mask;
+}
+
+/* ---------------------------------------------------------------------- */
+
+void FixWallHydrophobic::init()
+{
+  for (int m = 0; m < nwall; m++) {
+    if (xstyle[m] == VARIABLE) {
+      xindex[m] = input->variable->find(xstr[m]);
+      if (xindex[m] < 0)
+        error->all(FLERR,"Variable name for fix wall/hydrophobic does not exist");
+      if (!input->variable->equalstyle(xindex[m]))
+        error->all(FLERR,"Variable for fix wall/hydrophobic is invalid style");
+    }
+    if (estyle[m] == VARIABLE) {
+      eindex[m] = input->variable->find(estr[m]);
+      if (eindex[m] < 0)
+        error->all(FLERR,"Variable name for fix wall/hydrophobic does not exist");
+      if (!input->variable->equalstyle(eindex[m]))
+        error->all(FLERR,"Variable for fix wall/hydrophobic is invalid style");
+    }
+    if (sstyle[m] == VARIABLE) {
+      sindex[m] = input->variable->find(sstr[m]);
+      if (sindex[m] < 0)
+        error->all(FLERR,"Variable name for fix wall/hydrophobic does not exist");
+      if (!input->variable->equalstyle(sindex[m]))
+        error->all(FLERR,"Variable for fix wall/hydrophobic is invalid style");
+    }
+  }
+
+  // setup coefficients
+
+  for (int m = 0; m < nwall; m++) precompute(m);
+
+  if (strstr(update->integrate_style,"respa")) {
+    ilevel_respa = ((Respa *) update->integrate)->nlevels-1;
+    if (respa_level >= 0) ilevel_respa = MIN(respa_level,ilevel_respa);
+  }
+}
+
+/* ---------------------------------------------------------------------- */
+
+void FixWallHydrophobic::setup(int vflag)
+{
+  if (strstr(update->integrate_style,"verlet")) {
+    if (!fldflag) post_force(vflag);
+  } else {
+    ((Respa *) update->integrate)->copy_flevel_f(ilevel_respa);
+    post_force_respa(vflag,ilevel_respa,0);
+    ((Respa *) update->integrate)->copy_f_flevel(ilevel_respa);
+  }
+}
+
+/* ---------------------------------------------------------------------- */
+
+void FixWallHydrophobic::min_setup(int vflag)
+{
+  post_force(vflag);
+}
+
+/* ----------------------------------------------------------------------
+   only called if fldflag set, in place of post_force
+------------------------------------------------------------------------- */
+
+void FixWallHydrophobic::pre_force(int vflag)
+{
+  post_force(vflag);
+}
+
+/* ---------------------------------------------------------------------- */
+
+void FixWallHydrophobic::post_force(int vflag)
+{
+
+  // energy and virial setup
+
+  eflag = 0;
+  if (vflag) v_setup(vflag);
+  else evflag = 0;
+  for (int m = 0; m <= nwall; m++) ewall[m] = 0.0;
+
+  // coord = current position of wall
+  // evaluate variables if necessary, wrap with clear/add
+  // for epsilon/sigma variables need to re-invoke precompute()
+
+  if (varflag) modify->clearstep_compute();
+
+  double coord;
+  for (int m = 0; m < nwall; m++) {
+    if (xstyle[m] == VARIABLE) {
+      coord = input->variable->compute_equal(xindex[m]);
+      if (wallwhich[m] < YLO) coord *= xscale;
+      else if (wallwhich[m] < ZLO) coord *= yscale;
+      else coord *= zscale;
+    } else coord = coord0[m];
+    if (wstyle[m] == VARIABLE) {
+      if (estyle[m] == VARIABLE) {
+        epsilon[m] = input->variable->compute_equal(eindex[m]);
+        if (epsilon[m] < 0.0)
+          error->all(FLERR,"Variable evaluation in fix wall/hydrophobic gave bad value");
+      }
+      if (sstyle[m] == VARIABLE) {
+        sigma[m] = input->variable->compute_equal(sindex[m]);
+        if (sigma[m] < 0.0)
+          error->all(FLERR,"Variable evaluation in fix wall/hydrophobic gave bad value");
+      }
+      precompute(m);
+    }
+
+    wall_particle(m,wallwhich[m],coord);
+  }
+
+  if (varflag) modify->addstep_compute(update->ntimestep + 1);
+}
+
+/* ---------------------------------------------------------------------- */
+
+void FixWallHydrophobic::post_force_respa(int vflag, int ilevel, int iloop)
+{
+  if (ilevel == ilevel_respa) post_force(vflag);
+}
+
+/* ---------------------------------------------------------------------- */
+
+void FixWallHydrophobic::min_post_force(int vflag)
+{
+  post_force(vflag);
+}
+
+/* ----------------------------------------------------------------------
+   energy of wall interaction
+------------------------------------------------------------------------- */
+
+double FixWallHydrophobic::compute_scalar()
+{
+  // only sum across procs one time
+
+  if (eflag == 0) {
+    MPI_Allreduce(ewall,ewall_all,nwall+1,MPI_DOUBLE,MPI_SUM,world);
+    eflag = 1;
+  }
+  return ewall_all[0];
+}
+
+/* ----------------------------------------------------------------------
+   components of force on wall
+------------------------------------------------------------------------- */
+
+double FixWallHydrophobic::compute_vector(int n)
+{
+  // only sum across procs one time
+
+  if (eflag == 0) {
+    MPI_Allreduce(ewall,ewall_all,nwall+1,MPI_DOUBLE,MPI_SUM,world);
+    eflag = 1;
+  }
+  return ewall_all[n+1];
 }
--- src/fix_wall_hydrophobic.h
+++ src/fix_wall_hydrophobic.h
@@ -20,18 +20,51 @@ FixStyle(wall/hydrophobic,FixWallHydrophobic)
 #ifndef LMP_FIX_WALL_HYDROPHOBIC_H
 #define LMP_FIX_WALL_HYDROPHOBIC_H
 
-#include "fix_wall.h"
+#include "fix.h"
 
 namespace LAMMPS_NS {
 
-class FixWallHydrophobic : public FixWall {
+class FixWallHydrophobic : public Fix {
  public:
+  int nwall;
+  int wallwhich[6];
+  double coord0[6];
+  int xflag;           // 1 if any wall position is a variable
+  int xstyle[6];
+  int xindex[6];
+  char *xstr[6];
+
   FixWallHydrophobic(class LAMMPS *, int, char **);
+  virtual ~FixWallHydrophobic();
+  int setmask();
+  void init();
   void precompute(int);
   void wall_particle(int, int, double);
+  void setup(int);
+  void min_setup(int);
+  void pre_force(int);
+  void post_force(int);
+  void post_force_respa(int, int, int);
+  void min_post_force(int);
+  double compute_scalar();
+  double compute_vector(int);
 
- private:
+ protected:
+  int wallstyle[6];
+  char *varstr[6];
+  int varindex[6];
   double coeff1[6],coeff2[6],coeff3[6],coeff4[6],offset[6];
+  double lambda[6], sigmaS[6];
+  double epsilon[6],sigma[6],cutoff[6];
+  double ewall[7],ewall_all[7];
+  double xscale,yscale,zscale;
+  int estyle[6],sstyle[6],wstyle[6];
+  int eindex[6],sindex[6];
+  char *estr[6],*sstr[6];
+  int varflag;                // 1 if any wall position,epsilon,sigma is a var
+  int eflag;                  // per-wall flag for energy summation
+  int ilevel_respa;
+  int fldflag;
 };
 
 }
@@ -41,9 +74,34 @@ class FixWallHydrophobic : public FixWall {
 
 /* ERROR/WARNING messages:
 
-E: Particle on or inside fix wall surface
+E: Illegal ... command
 
-Particles must be "exterior" to the wall in order for energy/force to
-be calculated.
+Self-explanatory.  Check the input script syntax and compare to the
+documentation for the command.  You can use -echo screen as a
+command-line option when running LAMMPS to see the offending line.
+
+E: Wall defined twice in fix wall/hydrophobic command
+
+Self-explanatory.
+
+E: Cannot use fix wall/hydrophobic in periodic dimension
+
+Self-explanatory.
+
+E: Cannot use fix wall/hydrophobic zlo/zhi for a 2d simulation
+
+Self-explanatory.
+
+E: Variable name for fix wall/hydrophobic does not exist
+
+Self-explanatory.
+
+E: Variable for fix wall/hydrophobic is invalid style
+
+Only equal-style variables can be used.
+
+W: Should not allow rigid bodies to bounce off relecting walls
+
+LAMMPS allows this, but their dynamics are not computed correctly.
 
 */
